4a6e71c544d99e4581625fb62a6cc6ff8c63df0e
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-06-11 09:22:22 -0500

    Completed 257
    Created Tkinter GUI Timer program in Grid()

--- src/day_28_notes/257_mysolution.py
+++ src/day_28_notes/257_mysolution.py
@@ -19,11 +19,24 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-text = "✔"
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
 
+canvas.create_image(100, 112, image=tomato_img)
+canvas.grid(column=1, row=1)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+timer_label = Label(text="Timer", font=(FONT_NAME,30))
+timer_label.config(text="Timer", bg=YELLOW, fg=GREEN)
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", font=("Arial", 10, "bold"))
+#start_label.config(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", font=("Arial", 10, "bold"))
+#reset_label.config(text="Reset")
+reset_button.grid(column=2, row=2)
+
+canvas.grid()
 window.mainloop()
