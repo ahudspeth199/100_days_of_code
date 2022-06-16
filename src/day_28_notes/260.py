02e84bb8e77eb5fe90b933d004e31379d54b6f97
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-06-16 18:40:43 -0500

    Setting Different Timer Sessions and Values

--- src/day_28_notes/260.py
+++ src/day_28_notes/260.py
@@ -1,3 +1,4 @@
+# Setting Different Timer Sessions and Values
 from tkinter import *
 import math
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -23,10 +24,13 @@ def start_timer():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
