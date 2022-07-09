53725ad5faca8a39a65a405511fab9c64e6dc49c
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-07-09 14:14:48 -0500

    Challenge 1 - Handling Exceptions in the Password Manager

--- src/day_30_notes/276.py
+++ src/day_30_notes/276.py
@@ -47,14 +47,14 @@ def save():
         except FileNotFoundError:
             with open("data.json", "w") as data_file:
                 json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
 
-                # Updating old data with new data
-                data.update(new_data)
-
-        with open("data.json", "w") as data_file:
-                # Saving updated data
-                json.dump(data, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
