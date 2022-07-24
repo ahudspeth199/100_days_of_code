2b62e17847ce900642516db0fce07c195cf3fffb
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-07-23 22:39:57 -0500

    Trying email

--- src/day_32_notes/289.py
+++ src/day_32_notes/289.py
@@ -1,10 +1,16 @@
-import  smtplib
+import smtplib, ssl
 
-my_email = "python_test@adminmytech.com"
-password = "f7JjCRqYyy"
+my_email = input("Enter your email and press enter: ")
+password = input("Enter your password and press enter: ")
+message = "Python Email!!"
+port = 587
+receiver_email = "python_test@adminmytech.com"
 
-connection = smtplib.SMTP("smtp.gmail.com")
+context = ssl.create_default_context()
+
+connection = smtplib.SMTP_SSL("mail.gmail.com", port)
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="hudspeth199@yahoo.com", msg="Python Email!!")
+connection.sendmail(from_addr=my_email, to_addrs=receiver_email, msg=message)
 connection.close()
+
