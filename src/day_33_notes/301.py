69d3806b9dc747d4841b7586f45933b60d5d4405
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-08-13 11:20:32 -0500

    ISS Overhead Notifier Project- Challenge & Solution

--- src/day_33_notes/301.py
+++ src/day_33_notes/301.py
@@ -4,31 +4,55 @@
 # BONUS: run the code every 60s
 
 import requests
-import datetime as dt
-import random
+from datetime import datetime
+import smtplib
+import time
 
+my_email = input("Enter your email and press enter: ")
+password = input("Enter your password and press enter: ")
+receiver_email = "python_test@adminmytech.com"
+port = 587
 MY_LAT = 51.507351
 MY_LONG = -0.127758
 
-parameters = {
-        "lat": MY_LAT,
-        "lng": MY_LONG,
-        "formatted": 0,
-}
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
+def is_iss_overhead():
+    response = requests.get("https://api.sunrise-sunset.org/json")
+    response.raise_for_status()
+    data = response.json()
 
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-time_now = dt.now()
+    # Your position is within +5 or -5 degrees of the iss position.
+    if MY_LAT-5  <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <=MY_LONG+5:
+        return True
 
-now = dt.datetime.now()
-day_of_week = now.weekday()
-if day_of_week == 1:
-    with open(sunrise, sunset) as quotes:
-        lines = quotes.read().splitlines()
-        q_message = random.choice(lines)
+def is_night():
+    parameters = {
+            "lat": MY_LAT,
+            "lng": MY_LONG,
+            "formatted": 0,
+    }
 
-    import smtplib, ssl
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.mail.yahoo.com", port=port)
+        connection.starttls()
+        connection.login(my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=receiver_email,
+            msg="Subject: Look up\n\nThe ISS is above you in the sky."
+        )
