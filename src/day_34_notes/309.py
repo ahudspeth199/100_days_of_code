36bea719b9e9cecca3d7cda62e05b413d569fcf9
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-08-24 09:15:03 -0500

    Python Typing type Hints and Arrows

---
+++ src/day_34_notes/309.py
@@ -0,0 +1,23 @@
+# Data type
+# age: int
+# name: str
+# height: float
+# is_human: bool
+
+def police_check(age, int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return "They can drive"
+
+if police_check("twelve"):
+    print("You may pass")
+else:
+    print("Pay a fine")
+
+# Type Hints
+def greeting(name: str) -> str:
+    return 'Hello' + name
+
+
