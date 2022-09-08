6fd9e37ead70e0b6cc30817317df873b43fdf4ea
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-09-07 20:57:43 -0500

    315 challenge

---
+++ src/day_35_notes/challenge_315.py
@@ -0,0 +1,8 @@
+# Objective: print "Bring an Umbrella" if any of the next 12 hourly weather condition codes is less than 700.
+
+#HINT
+'''
+1. Practice printing out the weather ID for the weather in the 0th hour.
+2. Try to create a slice from the weather data to get a list of the hourly forecasts for only the next 12 hours.
+3. Using the above try to create a list of only the next 12 weather condition codes.
+'''
