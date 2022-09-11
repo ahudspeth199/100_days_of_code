05d309e55c52799d802303cee7ef5531fbc75aa4
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-09-11 14:10:52 -0500

    314Challenge - check if it will rain in the next 12 hours
    JSON passing and also understanding how to work with parameters in APIs

--- src/day_35_notes/challenge_315.py
+++ src/day_35_notes/challenge_315.py
@@ -6,3 +6,26 @@
 2. Try to create a slice from the weather data to get a list of the hourly forecasts for only the next 12 hours.
 3. Using the above try to create a list of only the next 12 weather condition codes.
 '''
+import requests
+
+OWM_Enpoint = "https://api.openweathermap.org/data/2.5/weather"
+api_key = "bdbd1a38facdc607a632669c78513d3b"
+
+weather_params = {
+    "lat": 33.804821,
+    "lon": -84.172440,
+    "appid": api_key,
+    "exclude": "current, minutely, daily"
+}
+
+response = requests.get(OWM_Enpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        print("Bring an umbrella.")
+
+
+#print(weather_data["hourly"][0]["weather"][0]["id"])
