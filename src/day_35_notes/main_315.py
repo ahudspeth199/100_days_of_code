4eff062c8f49674ef19690347d5adf06f36e35ea
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-09-13 22:02:08 -0500

    Sending SMS via the Twilio API
    able to receive an SMS when the weather condition
    
    in the next 12 hours

--- src/day_35_notes/main_315.py
+++ src/day_35_notes/main_315.py
@@ -1,8 +1,10 @@
 import requests
-from twilio.rest import client
+from twilio.rest import Client
 
 OWM_Enpoint = "https://api.openweathermap.org/data/2.5/weather"
 api_key = "bdbd1a38facdc607a632669c78513d3b"
+account_sid = "ACe8821ff8d631d56524b27a015e71179f"
+auth_token = ""
 
 weather_params = {
     "lat": 33.804821,
@@ -15,7 +17,20 @@ response = requests.get(OWM_Enpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
 for hour_data in weather_slice:
     condition_code = hour_data["weather"][0]["id"]
     if int(condition_code) < 700:
-        print("Bring an umbrella.")
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="Join Earth's mightest heros. L",
+        from="+9393928",
+        to="Your verified number"
+    )
+    print(message.status)
