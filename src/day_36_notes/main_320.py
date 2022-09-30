80b4e19cb1b32d41fc7909f926d179e501e4f398
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-09-29 21:52:17 -0500

    320 Start Walkthrough

--- src/day_36_notes/main_320.py
+++ src/day_36_notes/main_320.py
@@ -5,6 +5,8 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+STOCK_API_KEY = "4D69MWXCW85KCWS4"
+
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
