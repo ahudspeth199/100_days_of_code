19ec9e6d0ac1af01f672f119fcb8d1746c2acb8e
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-10-20 22:39:39 -0500

    325

--- src/day_37_notes/main_325.py
+++ src/day_37_notes/main_325.py
@@ -1,12 +1,22 @@
 import requests
 
+USERNAME = "hudspeth199"
+TOKEN = "kjskwnxi8b9w8i3o6"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "kjskwnxi8b9w8i3o6",
-    "username": "hudspeth199",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id":
+}
+
+requests.post()
