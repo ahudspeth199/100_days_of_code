267124f4a37753e11723b0b849ce122ad984d854
Anthony Hudspeth <88898585+ahudspeth199@users.noreply.github.com>
2022-11-09 20:25:27 -0600

    327

---
+++ src/day_37_notes/main_327.py
@@ -0,0 +1,43 @@
+import requests
+from datetime import datetime
+
+USERNAME = "hudspeth199"
+TOKEN = "kjskwnxi8b9w8i3o6"
+pixela_endpoint = "https://pixe.la/v1/users"
+GRAPH_ID = "graph1"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color":"ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+#response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+#print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": "20221108",
+    "quantity": "1",
+}
+
+response = requests.post(url=pixel_creation_endpoint,json=pixel_data, headers=headers)
+print(response.text)
